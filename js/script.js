e14b79c4648d4d4c4e64fc264807a904e3a2b587
Tim Lebedev <timlebedev2@gmail.com>
2021-08-03 21:39:12 +0300

    add_JS_file

---
+++ js/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+
+
+/* let userName = "Alex";
+let number = 10;
+let counter = 100;
+let $count = 10;
+let _count = 10;
+let bool = true; 
+console.log(typeof bool);
+console.log(typeof number);
+console.log(typeof userName);
+
+let firstName = 'Ivan';
+let lastName = 'Ivanov';
+let res = firstName +" "+ lastName;
+let result = `${firstName}   ${lastName}`;
+console.log(res);
+console.log(result); */
+
+
+
+//const userAge = 1990
+
+//var a = 555;
+
+ 
+//let integer = 46;
+///
+//let float = 46.85;
+
+//let million = "1000000";
+//let millionShort = 1e6;
+//console.log(million);
+//console.log(millionShort);
+
+//let small = 0.0000009;
+//let ultraSmall = 9e-6;
+//console.log(ultraSmall)
+
+//let user = "Alexander";
+
+//console.log(user);
+
+//let text = "Бренд компа 'Apple'";
+//console.log(text);//
+
+//let a = 10;
+//let b = 20;
+//console.log(a + b);
+
+// null
+//let total = null;
+//console.log(total);
+
+// undefined
+//let count;
+//console.log(count);
+
+//object
+//let cat = {
+  //  name: "Ulvia",
+  //  breed: "British",
+  //  age: 8,
+  //  color: "Black-Mramor"
+// }
+// console.log(cat);
+
+//let str = "10";
+//let digit = 12;
+//let result1 = str * digit;
+// console.log(result1);
+
+
+/* let z = 10;
+z += 12;
+console.log(z);
+
+alert(); */
+
+
+/* let userAge = prompt("Введите ваш возраст...");
+
+if (userAge >= 18){
+    console.log("Добро пожаловать");
+} */
+
